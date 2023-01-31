14eb578aef96972cfe669e3dbdd4fb34b899c90d
Benny Solomon <mr.benny.solomon@gmail.com>
2023-01-31 10:59:32 +0200

    24.1_is_square

--- 24.1_is_square/script.js
+++ 24.1_is_square/script.js
@@ -4,3 +4,11 @@ function Square(a, b, c, d){
   this.c = c;
   this.d = d;
   }
+
+  Square.prototype.isSquare = function () {
+    return this.b - this.a === this.d - this.c && this.c - this.a === this.d - this.b;
+  }
+
+  square = new Square(1, 2, 2, 3);
+
+  console.log(square.isSquare());
