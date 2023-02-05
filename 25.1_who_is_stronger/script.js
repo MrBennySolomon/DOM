fa40174f71ae6afaa3919c2295df2853647865a6
Benny Solomon <mr.benny.solomon@gmail.com>
2023-02-05 11:30:20 +0200

    25.1_who_is_stronger

--- 25.1_who_is_stronger/script.js
+++ 25.1_who_is_stronger/script.js
@@ -3,17 +3,19 @@
 const hero = {
   health: 5,
   power: 68,
-  getStrength: function(){
+  getStrength: function() {
     if (this.health <= 5){ 
       return this.power - 10; 
     } else 
       return this.power;
   } 
 }
-function whoIsStronger(getStrength){ const myStrength = 82;
-  if (getStrength() < myStrength){
+function whoIsStronger(getStrength){ 
+  const myStrength = 82;
+  if (this.getStrength() < myStrength){
     return "I am stronger";
   } else 
     return "You are stronger";
 }
-whoIsStronger(hero.getStrength);
+console.log(whoIsStronger.call(hero, hero.getStrength));
+//whoIsStronger(hero.getStrength);
