5591943a7a1083a48b082a1e090c50f956ddaa64
Benny Solomon <mr.benny.solomon@gmail.com>
2023-02-07 11:35:05 +0200

    30.2_movie_app

---
+++ 30.2_movie_app/script.js
@@ -0,0 +1,22 @@
+const btn = document.querySelector('button.btn');
+const input = document.querySelector('input.input');
+const user = document.querySelector('div.user');
+
+const fetchGithubUser = async () => {
+  const response = await fetch(`https://api.github.com/users/${input.value}`);
+  const data = await response.json();
+
+  user.innerHTML += `
+  <div class="info">
+    <img src="${data.avatar_url}" alt="${data.name}" width="100px" height="100px">
+    <p>${data.name} have ${data.public_repos} public repos </p>
+  </div>
+  `;
+};
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    fetchGithubUser();
+  }
+});
+btn.addEventListener("click", fetchGithubUser);
