55ad04203c9a22ffae76187979a723b31b4d63ef
MrBennySolomon <MrBennySolomon@gmail.com>
2023-02-08 14:31:08 +0200

    mini recap

--- mini_recap/5_event_listeners/script.js
+++ mini_recap/5_event_listeners/script.js
@@ -1,13 +1,16 @@
 // Exercise 1: Add a click event listener to the first box element
 // that logs "First box clicked" to the console when clicked
 // Make the callback function that is being passed in to event listener a named function
+const container = document.getElementById('container');
+const children =container.children;
+//children[0].addEventListener('click', function(e) { console.log('First box clicked')});
 
 
 // Exercise 2: Add a click event listener to the container element
 // that logs "Container clicked" to the console when clicked
 // Click on the first box and see which event listener is called first
 // Make the callback functions that are being passed in to event listeners named function
-
+container.addEventListener('click', function(e) { console.log('Container clicked')});
 
 // Exercise 3: Add the following code inside the first box event listener function:
 // event.stopPropagation();
@@ -15,12 +18,22 @@
 // Make the callback function that is being passed in to event listener of the Container a named function
 // and the function that is being passed in to the event listener of the first box an anonyms function
 
+//? children[0].addEventListener('click', function(e) { 
+//?     e.stopPropagation();
+//?     console.log('First box clicked')
+//? });
 
 // Exercise 4: Remove the event.stopPropagation() method from the first box element
 // Make the event listener run only once
-
+//? children[0].addEventListener('click', function(e) { 
+//?   console.log('First box clicked')
+//? }, {once : true});
 
 // Exercise 5: Use the removeEventListener method to remove the event listener from the first box element
+function removeListener() {
+  container.removeEventListener('click', onClick);
+  console.log('First box clicked')
+}
 
 
 // Exercise 6: Add a click event listener to the container element that logs the background-color
